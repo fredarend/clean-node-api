208c695f84294b71ea630167a34515f1345c83e6
Frederico Arend Binsfeld <frederico.binsfeld@mazzafc.tech>
2022-05-10 09:02:58 -0300

    test: ensure SignupControler returns 400 if no email is provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missig param: name'))
   })
+
+  test('Should be return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Any Name',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missig param: email'))
+  })
 })
